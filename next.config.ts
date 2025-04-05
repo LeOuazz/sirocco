c350c428918ab0e22f5cc832323112f33f98c56b
leouazz <leouazz@gmail.com>
2025-04-05 06:02:40 +0200

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    typescript: {
+        ignoreBuildErrors: true, // ✅ Ignore les erreurs TypeScript
+    },
+    eslint: {
+        ignoreDuringBuilds: true, // ✅ Ignore les erreurs ESLint
+    },
 };
+
 
 export default nextConfig;
